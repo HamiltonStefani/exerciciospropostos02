7c3aeb21ef88ffd9b2c625b60059d82cbb85e6dd
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2022-12-28 12:03:29 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package exerciciospropostos02;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N;
+		
+		System.out.println("Digite o número: ");
+		
+		N = sc.nextInt();
+		
+		if (N % 2 == 0) {
+			System.out.println("O número é par");
+		}else {
+			System.out.println("O número é Impar");
+		}
+		
+		
+		
+		sc.close();
+
+	}
+
+}
